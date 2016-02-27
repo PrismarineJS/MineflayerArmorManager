a7744e281548b32e5fc58125b78e1bf59456d140
Konstantin Azizov <azizovkostya97@gmail.com>
2016-02-27 13:08:46 +0200

    Rename destination object
    Also add docs for isArmor and sorted armor ids

--- data/armor.js
+++ data/armor.js
@@ -1,10 +1,10 @@
 module.exports = {
   ids: {
     lether: 298,
-    chainmail: 302,
+    golden: 314,
     iron: 306,
-    diamond: 310,
-    golden: 314
+    chainmail: 302,
+    diamond: 310
   },
-  destionations: ['head', 'torso', 'legs', 'feet']
+  destinations: ['head', 'torso', 'legs', 'feet']
 };
