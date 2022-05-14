aaf530ab9c4079d91a13198ef5b65e3427c48e18
u9g <git@u9g.dev>
2022-05-14 18:30:34 -0400

    use collected.getDroppedItem() in playerCollect and dont use removed func findItemOrBlockById

--- src/lib/isArmor.ts
+++ src/lib/isArmor.ts
@@ -1,11 +1,10 @@
 import { TypeDestination, offhandMaterials } from "../data/armor";
+import { Item } from "prismarine-item";
 import { IndexedData } from "../types";
 
 const armorTypes = Object.keys(TypeDestination);
 
-export const isArmor = (itemId: number, versionData: IndexedData): boolean => {
-  const item = versionData.findItemOrBlockById(itemId);
-
+export const isArmor = (item: Item): boolean => {
   return item && (armorTypes.some((type) => item.name.endsWith(type))
                 || offhandMaterials.some((type) => item.name === type));
 };
